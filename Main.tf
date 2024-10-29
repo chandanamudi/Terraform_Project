d101a96bd6bc6d2af80de41546881acfc11aab64
chandanamudi <50474898+chandanamudi@users.noreply.github.com>
2024-10-29 13:35:23 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,42 +1,13 @@
 provider "aws" {
-  region = "ap-south-1"  # Change to your desired region
+  region = "ap-south-1"                   # Change to your desired region
   access_key = "YOUR_AWS_ACCESS_KEY"
   secret_key = "YOUR_AWS_SECRET_KEY"
 }
 
-resource "aws_security_group" "my-security-group123" {
-  name        = "my-security-group123"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # All inbound traffic 
-    #Change this to a more restrictive CIDR block for production
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # Allows all outbound traffic
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  }
-
 resource "aws_instance" "my_ec2_instance123" {
-  ami           = var.ami_value  # Change to your desired AMI
-  instance_type = var.instance_type  # Change to your desired instance type
-  security_groups = [aws_security_group.my-security-group123.name]
-  key_name = var.key_name
+  ami           = "ami-0dee22c13ea7a9a67" # Change to your desired AMI
+  instance_type =  "t2.micro"             # Change to your desired instance type
+  key_name = "Ajaykumar12334"
 
   tags = {
     Name = "my_ec2_instance123"
